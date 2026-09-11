bfa00a2c16eff2cb54675b29545c26ab2323166d
CamilaQuintanilla11 <camilaquintanillacabrera@gmail.com>
2026-09-10 20:24:26 -0600

    Documentación auth.guard.ts

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,6 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
 import { verify } from './jwt';
 
+/**
+ * AuthGuard es una función que protege las rutas de la API implementando JWT de acceso.
+ * implementa CanActivate, que es una interfaz de NestJS que regresa un booleano indicando si la ruta puede ser accedida.
+ * utiliza {@link verify} para validar, y deja el payload del token en req.user para que pueda ser usado en los controladores.
+ * 
+ * 
+ * Si el token no es válido, regresa un UnauthorizedException 401
+ * 
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
